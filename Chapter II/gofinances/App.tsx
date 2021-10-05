ca47ff1ad9b859d5b4baa4132ec762aafa5f2653
Raul Sigoli <raul_sigoli@hotmail.com>
2021-10-05 15:32:09 -0300

    Adicionando manualmente o Typescript

--- Chapter II/gofinances/App.tsx
+++ Chapter II/gofinances/App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Welcome } from './src/compoments/Welcome';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome title="Hello, Raul Sigoli, Tudo Bem ? "/>
       <StatusBar style="auto" />
     </View>
   );
