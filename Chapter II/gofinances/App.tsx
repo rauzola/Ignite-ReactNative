c6c469eb074d28095691a5f992774a08e31ae3fe
Raul Sigoli <raul_sigoli@hotmail.com>
2021-10-06 13:59:37 -0300

    Criando Componentes com o Styled Components

--- Chapter II/gofinances/App.tsx
+++ Chapter II/gofinances/App.tsx
@@ -1,23 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard } from './src/screens/Dashboard';
 
-import { Welcome } from './src/compoments/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Welcome title="Hello, Raul Sigoli, Tudo Bem ? "/>
-      <StatusBar style="auto" />
-    </View>
+      <Dashboard />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
