90f966e6f823ddb8188790c8ec0c000818a55d9b
Raul Sigoli <raul_sigoli@hotmail.com>
2021-10-08 11:05:22 -0300

    Utilizando proporções em medidas

--- Chapter II/gofinances/src/screens/Dashboard/index.tsx
+++ Chapter II/gofinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/57502280?v=4' }}
+
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Raul</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+
+            </Header>
         </Container>
     )
 }
