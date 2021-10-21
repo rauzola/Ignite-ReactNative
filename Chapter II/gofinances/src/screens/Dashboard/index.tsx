6b995938976b2bebe66de2fbe1684669c0389948
Raul Sigoli <raul_sigoli@hotmail.com>
2021-10-21 11:15:15 -0300

    Deixando os cartões de transações dinâmicos

--- Chapter II/gofinances/src/screens/Dashboard/index.tsx
+++ Chapter II/gofinances/src/screens/Dashboard/index.tsx
@@ -24,6 +24,7 @@ import {
 export function Dashboard() {
     const data = [
         {
+        type: "positive",
         title:"Desenvolvimento de site",
         amount:"R$12.000,00",
         category: {
@@ -31,22 +32,26 @@ export function Dashboard() {
             icon: 'dollar-sign'
         },
         date:"13/04/2021"
-    },{
-        title:"Desenvolvimento de site",
-        amount:"R$12.000,00",
+    },
+    {
+        type: "negative",
+        title:"Hamburgueria Pizzy",
+        amount:"R$ 59,00",
         category: {
-            name: 'Vendas',
-            icon: 'dollar-sign'
+            name: 'Alimentação',
+            icon: 'coffee'
         },
-        date:"13/04/2021"
-    },{
-        title:"Desenvolvimento de site",
-        amount:"R$12.000,00",
+        date:"10/04/2021"
+    },
+    {
+        type: "negative",
+        title:"Aluguel do apartamento",
+        amount:"R$ 1.200,00",
         category: {
-            name: 'Vendas',
-            icon: 'dollar-sign'
+            name: 'Casa',
+            icon: 'shopping-bag'
         },
-        date:"13/04/2021"
+        date:"10/04/2021"
     },
 ];
 
