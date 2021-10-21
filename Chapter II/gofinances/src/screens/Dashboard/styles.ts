26f99670cd81dfdd911840c3a19941bb86a13f6d
Raul Sigoli <raul_sigoli@hotmail.com>
2021-10-21 11:28:22 -0300

    Finalizando a listagem de transações

--- Chapter II/gofinances/src/screens/Dashboard/styles.ts
+++ Chapter II/gofinances/src/screens/Dashboard/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from '.';
 
 export const Container = styled.View`
         flex: 1;
@@ -92,4 +95,11 @@ export const Title = styled.Text`
     margin-bottom: 16px;
 `;
 
-export const TransactionsList = styled.FlatList``;
+export const TransactionsList = styled(
+    FlatList as new () => FlatList<DataListProps>
+    ).attrs({
+    showsVerticalScrollIndicator: false,
+    contentContainerStyle:{
+        paddingBottom: getBottomSpace() 
+    }
+})``;
