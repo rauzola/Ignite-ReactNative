fd770838e3df22335a129c7b641849edd1e31df7
Raul Sigoli <raul_sigoli@hotmail.com>
2021-11-29 18:19:55 -0300

    Utilizando a Tab Navigation

--- Chapter II/gofinances/App.tsx
+++ Chapter II/gofinances/App.tsx
@@ -11,7 +11,9 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
+
 
 
 export default function App() {
@@ -28,7 +30,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <Register />
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
       </ThemeProvider>
     </>
   );
